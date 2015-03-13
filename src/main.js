91161f968bd72e8a68364db741328b5e91fb7da9
minimo <minimo.mobile@gmail.com>
2015-03-13 17:54:38 +0900

    フレームレート変更

--- src/main.js
+++ src/main.js
@@ -37,7 +37,7 @@ MEDIA_LLA = 2;      //LawLatencyAudioPlugin
 MEDIA_DEFAULT = MEDIA_ASSET;
 
 //フレームレート
-fps = 60;
+fps = 30;
 var sec = function(s) { return ~~(fps * s);}    //秒からフレーム数へ変換
 
 //表示レイヤー
